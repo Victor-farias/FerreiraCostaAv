4890b2c0eb175353a32e71769af8c0f2fba01c4b
Victor Farias <vfarias@cambridgeassociates.com>
2023-09-21 01:19:51 -0300

    UIpdating tables with some adjustments

--- FerreiraCostaAv/Migrations/20230921041305_LastAdjustments.Designer.cs
+++ FerreiraCostaAv/Migrations/20230921041305_LastAdjustments.Designer.cs
@@ -4,14 +4,16 @@ using FerreiraCostaAv.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FerreiraCostaAv.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230921041305_LastAdjustments")]
+    partial class LastAdjustments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FerreiraCostaAv/Migrations/20230921041305_LastAdjustments.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FerreiraCostaAv.Migrations
+{
+    public partial class LastAdjustments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Credentials_CredencialsId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "CredencialsId",
+                table: "Users",
+                newName: "CredentialId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_CredencialsId",
+                table: "Users",
+                newName: "IX_Users_CredentialId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Credentials_CredentialId",
+                table: "Users",
+                column: "CredentialId",
+                principalTable: "Credentials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Credentials_CredentialId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "CredentialId",
+                table: "Users",
+                newName: "CredencialsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_CredentialId",
+                table: "Users",
+                newName: "IX_Users_CredencialsId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Credentials_CredencialsId",
+                table: "Users",
+                column: "CredencialsId",
+                principalTable: "Credentials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
