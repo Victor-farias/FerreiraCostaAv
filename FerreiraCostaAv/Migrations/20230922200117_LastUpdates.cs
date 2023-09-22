e7d78e4f89defdb45f2678c327c41a3a0c28f6f0
Victor Farias <vfarias@cambridgeassociates.com>
2023-09-22 17:01:48 -0300

    Last migration

---
+++ FerreiraCostaAv/Migrations/20230922200117_LastUpdates.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FerreiraCostaAv.Migrations
+{
+    public partial class LastUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
